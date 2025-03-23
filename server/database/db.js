9bf093938c17a9b910528aeda6b1436eafdbfb35
Aashutosh20233325 <fitjeeaim6837@gmail.com>
2025-03-23 19:39:03 +0530

    Server initalistion and connect with mongoDB database

---
+++ server/database/db.js
@@ -0,0 +1,10 @@
+import mongoose from 'mongoose';
+const connectDB = async () =>{
+    try{
+        await mongoose.connect(process.env.MONGO_URI);
+        console.log("MongoDB conneected");
+    }catch(error){
+        console.log("error",error);
+    }
+}
+export default connectDB;
