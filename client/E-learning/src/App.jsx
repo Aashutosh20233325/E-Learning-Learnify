a891776465c6b1e19686192f9570ea99b2637ce3
Sneha <itschaudhary@gmail.com>
2025-03-24 22:44:33 +0530

    build Hero page

--- client/E-learning/src/App.jsx
+++ client/E-learning/src/App.jsx
@@ -1,15 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-import Login from "./pages/Login.jsx"
-function App() {
-  const [count, setCount] = useState(0)
+import HeroSection from './pages/student/HeroSection.jsx';
 
+import Login from "./pages/Login.jsx";
+function App() {
+  
   return (
-    <>
+    <main>
+    <HeroSection/>
      <Login/>
-    </>
+     
+    </main>
   )
 }
 
