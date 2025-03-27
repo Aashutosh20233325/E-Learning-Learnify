4f1ddeda1ef23def2e59b32d38da593de9f1ec13
Sneha <itschaudhary@gmail.com>
2025-03-27 21:48:31 +0530

    Resolved conflicts

--- server/controllers/course.controller.js
+++ server/controllers/course.controller.js
@@ -46,6 +46,79 @@ export const getCreatorCourses = async (req,res) => {
         })
     }
 }
+export const createLecture = async (req,res) => {
+    try {
+        const {lectureTitle} = req.body;
+        const {courseId} = req.params;
+
+        if(!lectureTitle || !courseId){
+            return res.status(400).json({
+                message:"Lecture title is required"
+            })
+        };
+
+        // create lecture
+        const lecture = await Lecture.create({lectureTitle});
+
+        const course = await Course.findById(courseId);
+        if(course){
+            course.lectures.push(lecture._id);
+            await course.save();
+        }
+
+        return res.status(201).json({
+            lecture,
+            message:"Lecture created successfully."
+        });
+
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({
+            message:"Failed to create lecture"
+        })
+    }
+}
+export const getCourseLecture = async (req,res) => {
+    try {
+        const {courseId} = req.params;
+        const course = await Course.findById(courseId).populate("lectures");
+        if(!course){
+            return res.status(404).json({
+                message:"Course not found"
+            })
+        }
+        return res.status(200).json({
+            lectures: course.lectures
+        });
+
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({
+            message:"Failed to get lectures"
+        })
+    }
+}
+export const getCourseById = async (req,res) => {
+    try {
+        const {courseId} = req.params;
+
+        const course = await Course.findById(courseId);
+
+        if(!course){
+            return res.status(404).json({
+                message:"Course not found!"
+            })
+        }
+        return res.status(200).json({
+            course
+        })
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({
+            message:"Failed to get course by id"
+        })
+    }
+}
 export const editCourse = async (req,res) => {
     try {
         const courseId = req.params.courseId;
@@ -85,23 +158,4 @@ export const editCourse = async (req,res) => {
         })
     }
 }
-export const getCourseById = async(req,res) =>{
-    try{
-        const {courseId} = req.params;
-        const course = await Course.findById(courseId);
-        if(!course){
-            return res.status(404).json({
-                message:"Course not found!"
-            })
 
-        }
-        return res.status(200).json({
-            course
-        })
-    }catch(error){
-        console.log(error);
-        return res.status(500).json({
-            message:"Failed to get Course by id"
-        })
-    }
-}
