c375f377c7059c966dbbdef9ee631c5b76bfe94d
Sneha <itschaudhary@gmail.com>
2025-03-30 05:52:42 +0530

    Search course functionality,Filter the courses on basis of category and prices,explore courses functionality added

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -54,36 +54,17 @@ export const login = async(req,res)=>{
                 success:false,
                 message:"Incorrect email or password"
             })
-            }
-            const isPasswordMatch=await bcrypt.compare(password,user.password);
-            if(!isPasswordMatch){
-                return res.status(400).json({
-                    success:false,
-                    message:"Incorrect email or password"
-                });
-
-            }
-            generateToken(res, user, `Welcome back ${user.name}`);
-
-
-
-            if(user.photoUrl){
-                const publicId = user.photoUrl.split("/").pop().split(".")[0]; // extract public id
-                deleteMediaFromCloudinary(publicId);
-            }
-
-            const cloudResponse = await uploadMedia(profilePhoto.path);
-            const photoUrl = cloudResponse.secure_url;
-    
-            const updatedData = {name, photoUrl};
-            const updatedUser = await User.findByIdAndUpdate(userId, updatedData, {new:true}).select("-password");
-
-            return res.status(200).json({
-                success:true,
-                user:updatedUser,
-                message:"Profile updated successfully."
-            })
         }
+        const isPasswordMatch=await bcrypt.compare(password,user.password);
+        if(!isPasswordMatch){
+            return res.status(400).json({
+                success:false,
+                message:"Incorrect email or password"
+            });
+        }
+        generateToken(res, user, `Welcome back ${user.name}`);
+        
+    }
     catch(error){
         console.log(error);
         return res.status(500).json({
@@ -110,19 +91,18 @@ export const login = async(req,res)=>{
  }
  export const getUserProfile=async(req,res)=>{
     try{
-      const userId=req.id;
-      const user=await User.findById(userId).select("-password");
-      if(!user){
+    const userId=req.id;
+    const user = await User.findById(userId).select("-password");
+    if(!user){
         return res.status(404).json({
             message:"Profile not found",
             success:false
         })
-      }
-
-      return res.status(200).json({
-           success:true,
-           user
-      })
+    }
+    return res.status(200).json({
+        success:true,
+        user
+    })
         
     }
     catch(error){
@@ -131,10 +111,8 @@ export const login = async(req,res)=>{
             success:false,
             message:"Failed to logout"
         })
-
     }
-
-    }
+}
     export const updateProfile = async (req,res) => {
         try {
             const userId = req.id;
