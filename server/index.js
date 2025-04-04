7c16dde39e0e9f04e0e13d23f1eecda66b7c3fd7
Aashutosh20233325 <fitjeeaim6837@gmail.com>
2025-04-04 10:46:13 +0530

    folder structure for deploying

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./database/db.js";
 import userRoute from "./routes/user.route.js"; 
+import path from "path";
 dotenv.config();
 import  cookieParser from "cookie-parser";
 import cors from "cors";
@@ -14,6 +15,8 @@ connectDB();
 const app = express();
 //const PORT = 8080
 const PORT = process.env.PORT || 3000;
+
+const _dirname = path.resolve();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
@@ -31,6 +34,10 @@ app.get("/home",(_,res)=>{
         message:"hello i m coming from backend"
      })
 
+})
+app.use(express.static(path.join(_dirname,"/client/E-learning/dist")))
+app.get('*',(_,res) =>{
+    res.sendFile(path.resolve(_dirname,'client/E-learning','dist','index.html'));
 })
 app.listen(PORT , () =>{
     console.log(`Server listen at port ${PORT}`);
